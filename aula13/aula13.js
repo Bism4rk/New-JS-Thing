984dea42eeb27561af99184a53a4128e4d091f4f
Bismarck <brunowasborn2rock@gmail.com>
2023-12-12 16:16:16 -0300

    if and else if

---
+++ aula13/aula13.js
@@ -0,0 +1,10 @@
+let clima = 'Chuva'
+let energia = 100
+
+if(energia > 70 && clima == 'Sol'){
+    console.log('Vou a praia!')
+}
+else{
+    console.log('Vou ao cinema!')
+}
+console.log('Fim do programa.')
