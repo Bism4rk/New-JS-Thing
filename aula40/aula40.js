87be485da3cf272dd6a0ba457a157f11be6ea24f
Bismarck <brunowasborn2rock@gmail.com>
2024-01-06 16:13:19 -0300

    create element

---
+++ aula40/aula40.js
@@ -0,0 +1,12 @@
+const caixa1 = document.querySelector('#caixa1')
+const btn_c = [...document.querySelectorAll('.curso')]
+const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React', 'MySQL', 'ReactNative']
+
+
+cursos.map((el,i)=>{
+    const novoElemento = document.createElement('div')
+    novoElemento.setAttribute(`id`, `c${i}`)
+    novoElemento.setAttribute('class','curso c1')
+    novoElemento.innerHTML = el
+    caixa1.appendChild(novoElemento)
+})
