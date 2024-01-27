90315066b3c7b2a5536419a7ef17b2d2e998c66f
Bismarck <brunowasborn2rock@gmail.com>
2024-01-27 15:59:54 -0300

    OBJECTS

---
+++ aula60/aula60.js
@@ -0,0 +1,42 @@
+const Pessoa = {
+    nome:'',
+    idade:'',
+    getNome:function(){
+        return this.nome
+    },
+    getIdade:function(){
+        return this.idade
+    },
+    setNome:function(nome){
+        this.nome = nome
+    },
+    setIdade:function(idade){
+        this.idade = idade
+    }
+}
+
+let pessoas = []
+const btn_add = document.querySelector('#btn_add')
+const res = document.querySelector('.res')
+
+const addPessoa = ()=>{
+    res.innerHTML = ''
+    pessoas.map((p)=>{
+        const div = document.createElement('div')
+        div.setAttribute('class', 'pessoa')
+        div.innerHTML = `Nome: ${p.getNome()} <br> Idade: ${p.getIdade()}`
+        res.appendChild(div)
+    })
+}
+
+btn_add.addEventListener('click', (evt)=>{
+    const nome = document.querySelector('#f_nome')
+    const idade = document.querySelector('#f_idade')
+    Pessoa.setNome(nome.value)
+    Pessoa.setIdade(idade.value)
+    pessoas.push(Pessoa)
+    nome.value = ''
+    idade.value = ''
+    nome.focus()
+    addPessoa()
+})
