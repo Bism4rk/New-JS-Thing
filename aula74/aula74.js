eecdeb8ffa97aaa3fbca19a203e24af30d124536
Bismarck <brunowasborn2rock@gmail.com>
2024-02-10 15:44:48 -0300

    promise

---
+++ aula74/aula74.js
@@ -0,0 +1,26 @@
+const numero = document.getElementById('numero')
+
+let promise = new Promise((res, rej) => {
+    let resultado = true
+    let tempo = 3000
+    setTimeout(() => {
+        if (resultado) {
+            res('Deu tudo certo!')
+        } else {
+            rej('Deu tudo errado...')
+        }
+    }, tempo)
+});
+
+promise.then((retorno)=>{
+    numero.innerHTML = retorno
+    numero.classList.remove('erro')
+    numero.classList.add('ok')
+})
+promise.catch((retorno)=>{
+    numero.innerHTML = retorno
+    numero.classList.remove('ok')
+    numero.classList.add('erro')
+})
+
+numero.innerHTML = 'Processando...'
