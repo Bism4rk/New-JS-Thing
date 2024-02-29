ae3c9bd59cee88f8ac7c35d21c753eb816bbeb5a
Bismarck <brunowasborn2rock@gmail.com>
2024-02-29 15:47:50 -0300

    string

---
+++ aula100/aula100.js
@@ -0,0 +1,13 @@
+let nome = new String('Bruno')
+let canal = new String('CFB Cursos')
+
+console.log(nome)
+console.log(typeof(nome))
+
+console.log(nome.charAt(0))
+console.log(nome.charCodeAt(0))
+
+console.log(nome.concat(canal))
+
+console.log(canal);
+console.log(canal.lastIndexOf('C'));
