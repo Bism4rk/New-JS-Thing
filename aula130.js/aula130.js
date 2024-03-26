e894bd0615f01c7a216fe04d6a84f706f964bcfd
Bismarck <brunowasborn2rock@gmail.com>
2024-03-26 16:24:04 -0300

    more clock

---
+++ aula130.js/aula130.js
@@ -0,0 +1,21 @@
+const timer = document.querySelector('#timer')
+const tmpini = Date.now()
+
+const contador = () =>{
+    const tmpatual = Date.now()
+    let cont = tmpatual - tmpini
+    let seg = Math.floor((tmpatual - tmpini) / 1000)
+    timer.innerHTML = converter(seg)
+}
+
+
+const converter = (seg) =>{
+    const hora = Math.floor(seg / 3600)
+    let resto = seg % 3600
+    const minuto = Math.floor(resto / 60)
+    const segundo = Math.floor(resto % 60)
+    const form = `${(hora<10?"0"+hora:hora)}:${(minuto<10?"0"+minuto:minuto)}:${(segundo<10?"0"+segundo:segundo)}`
+    return form
+}
+
+const intervalo = setInterval(contador, 1000)
