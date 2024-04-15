4650b5b9428e5d98ddea26d627ff5c17f8e95d84
Bismarck <brunowasborn2rock@gmail.com>
2024-04-15 15:35:16 -0300

    online 2

---
+++ aula151/aula151.js
@@ -0,0 +1,21 @@
+import { Cxmsg } from "./cxmsg.js";
+// import { Login } from "./login.js";
+
+const callback_ok = ()=>{}
+
+const callback_naook = ()=>{
+    const config = {
+        cor: 'red',
+        tipo: 'ok',
+        textos: '',
+        comando_sn: null
+    }
+    Cxmsg.mostrar(config, 'Erro!', 'Log-in não efetuado - Usuário ou senha errados!')
+}
+
+const configlogin = {
+    cor: '048',
+    img: 'logo.png',
+    endpoint: 'https://6573c537-7d92-4414-a2d0-76ec13fdfc3d-00-39qe15kjmc8b5.worf.replit.dev'
+}
+Login.login(null, callback_naook, configlogin)
