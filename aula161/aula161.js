a69580144d38af3bd170968f2031f9c7429cf3f4
Bismarck <brunowasborn2rock@gmail.com>
2024-04-26 15:57:31 -0300

    new thing again wow

--- aula161/aula161.js
+++ aula161/aula161.js
@@ -6,3 +6,8 @@ const btn_novo = document.querySelector('#btn_novo')
 const btn_pesquisar = document.querySelector('#btn_pesquisar')
 const btn_gestao = document.querySelector('#btn_gestao')
 const btn_sobre = document.querySelector('#btn_sobre')
+
+
+btn_home.addEventListener('click', (evt)=>{
+    alert('Boo!')
+})
