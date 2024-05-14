0d12a7783425be4d65acf60dc39af80a67a716b1
Bismarck <brunowasborn2rock@gmail.com>
2024-05-14 12:19:19 -0300

    more searching

---
+++ aula169/pesquisar.js
@@ -0,0 +1,20 @@
+const btn_pesq = document.querySelector('#btn_pesq')
+const f_txtpesq = document.querySelector('#f_txtpesq')
+
+btn_pesq.addEventListener('click', (evt)=>{
+    const valorpesq = f_txtpesq.value
+    if (valorpesq == "") {
+        alert('Digite a pesquisa!')
+        f_txtpesq.focus() 
+        return;
+    }
+    const f_pesq = document.querySelector("input[name=f_pesq]:checked").value;
+    console.log(f_pesq);
+    let endpoint =`http://localhost:1880/pesquisarcontatos/${f_pesq}/${valorpesq}`
+    fetch(endpoint)
+    .then(res=>res.json())
+    .then(res=>{
+        console.log(res);
+    })
+})    
+
