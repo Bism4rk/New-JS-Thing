2bd3ec6e5a706998a22451dfdd3f0a3f77c4e20f
Bismarck <brunowasborn2rock@gmail.com>
2024-05-19 15:51:27 -0300

    more searching

--- aula171/pesquisar.js
+++ aula171/pesquisar.js
@@ -1,7 +1,12 @@
 const btn_pesq = document.querySelector('#btn_pesq')
 const f_txtpesq = document.querySelector('#f_txtpesq')
+const dados = document.querySelector('#dados')
+
+console.log('THE SHADOWS OF OUR PAST AND THIS IS THE MOMENT IT ALL COMES DOOOOOOOOOWN')
+
 
 btn_pesq.addEventListener('click', (evt)=>{
+    dados.innerHTML = ""
     const valorpesq = f_txtpesq.value
     if (valorpesq == "") {
         alert('Digite a pesquisa!')
@@ -14,6 +19,37 @@ btn_pesq.addEventListener('click', (evt)=>{
     fetch(endpoint)
     .then(res=>res.json())
     .then(res=>{
+        res.forEach(el => {
+            const linha = document.createElement('div')
+            linha.setAttribute('class', 'linhaDados')
+
+            const c1 = document.createElement('div')
+            c1.setAttribute('class', 'coluna c1')
+            c1.innerHTML = el.n_contato_contato
+            linha.appendChild(c1)
+
+            const c2 = document.createElement('div')
+            c2.setAttribute('class', 'coluna c2')
+            c2.innerHTML = el.s_nome_contato
+            linha.appendChild(c2)
+
+            const c3 = document.createElement('div')
+            c3.setAttribute('class', 'coluna c3')
+            c3.innerHTML = el.s_celular_contato
+            linha.appendChild(c3)
+
+            const c4 = document.createElement('div')
+            c4.setAttribute('class', 'coluna c4')
+            c4.innerHTML = el.s_email_contato
+            linha.appendChild(c4)
+
+            const c5 = document.createElement('div')
+            c5.setAttribute('class', 'coluna c5')
+            c5.innerHTML = el.dt_dtnasc_contato
+            linha.appendChild(c5)
+
+            dados.appendChild(linha)
+        });
         console.log(res);
     })
 })    
