80b3bc2f843c0543de91950f12a1504c712f9b9d
Bismarck <brunowasborn2rock@gmail.com>
2024-08-04 16:13:03 -0300

    uijn

---
+++ aula245/backoffice/index.js
@@ -0,0 +1,4 @@
+const login = document.querySelector('#login')
+const f_username = document.querySelector('#f_username')
+const f_senha = document.querySelector('#f_senha')
+const btn_login = document.querySelector('#btn_login')
